0b5450bd7006defb5313de3b272172f777f6c76d
Yvantrey <rugambayvan1@gmail.com>
2026-08-08 14:11:14 +0200

    Add core AR tracking and letter panel scripts

---
+++ Assets/Scripts/AlphabetImageTracker.cs
@@ -0,0 +1,133 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class AlphabetImageTracker : MonoBehaviour
+{
+    [Header("AR Tracking")]
+    public ARTrackedImageManager trackedImageManager;
+
+    [Header("AR Panel")]
+    public GameObject letterPanelPrefab;
+
+    [Header("Panel Position")]
+    public Vector3 panelOffset = new Vector3(0f, 0.15f, 0f);
+
+    private Dictionary<TrackableId, GameObject> panels =
+        new Dictionary<TrackableId, GameObject>();
+
+    private void OnEnable()
+    {
+        if (trackedImageManager != null)
+        {
+            trackedImageManager.trackablesChanged.AddListener(OnTrackedImagesChanged);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (trackedImageManager != null)
+        {
+            trackedImageManager.trackablesChanged.RemoveListener(OnTrackedImagesChanged);
+        }
+    }
+
+    private void OnTrackedImagesChanged(
+        ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
+        {
+            CreatePanel(trackedImage);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            UpdatePanel(trackedImage);
+        }
+
+        foreach (KeyValuePair<TrackableId, ARTrackedImage> pair in eventArgs.removed)
+        {
+            RemovePanel(pair.Value);
+        }
+    }
+
+    private void CreatePanel(ARTrackedImage trackedImage)
+    {
+        if (letterPanelPrefab == null)
+        {
+            Debug.LogError("AlphabetImageTracker: Letter Panel Prefab is missing.");
+            return;
+        }
+
+        if (panels.ContainsKey(trackedImage.trackableId))
+            return;
+
+        GameObject panel = Instantiate(
+            letterPanelPrefab,
+            trackedImage.transform
+        );
+
+        panel.transform.localPosition = panelOffset;
+        panel.transform.localRotation = Quaternion.identity;
+
+        LetterPanel letterPanel = panel.GetComponent<LetterPanel>();
+
+        if (letterPanel != null)
+        {
+            string letter = trackedImage.referenceImage.name;
+            letterPanel.ShowLetter(letter);
+        }
+        else
+        {
+            Debug.LogError(
+                "AlphabetImageTracker: LetterPanel prefab does not contain LetterPanel.cs."
+            );
+        }
+
+        panels.Add(trackedImage.trackableId, panel);
+
+        UpdatePanel(trackedImage);
+    }
+
+    private void UpdatePanel(ARTrackedImage trackedImage)
+    {
+        if (!panels.TryGetValue(
+                trackedImage.trackableId,
+                out GameObject panel))
+        {
+            return;
+        }
+
+        bool isTracking =
+            trackedImage.trackingState == TrackingState.Tracking;
+
+        panel.SetActive(isTracking);
+
+        if (isTracking)
+        {
+            panel.transform.localPosition = panelOffset;
+            panel.transform.localRotation = Quaternion.identity;
+
+            LetterPanel letterPanel =
+                panel.GetComponent<LetterPanel>();
+
+            if (letterPanel != null)
+            {
+                string letter = trackedImage.referenceImage.name;
+                letterPanel.ShowLetter(letter);
+            }
+        }
+    }
+
+    private void RemovePanel(ARTrackedImage trackedImage)
+    {
+        if (panels.TryGetValue(
+                trackedImage.trackableId,
+                out GameObject panel))
+        {
+            Destroy(panel);
+            panels.Remove(trackedImage.trackableId);
+        }
+    }
+}
---
+++ Assets/Scripts/LetterPanel.cs
@@ -0,0 +1,136 @@
+using TMPro;
+using UnityEngine;
+
+public class LetterPanel : MonoBehaviour
+{
+    private TMP_Text letterText;
+    private TMP_Text wordsText;
+
+    private void Awake()
+    {
+        TMP_Text[] texts = GetComponentsInChildren<TMP_Text>(true);
+
+        foreach (TMP_Text text in texts)
+        {
+            if (text.gameObject.name == "LetterText")
+            {
+                letterText = text;
+            }
+            else if (text.gameObject.name == "WordsText")
+            {
+                wordsText = text;
+            }
+        }
+
+        if (letterText == null)
+        {
+            Debug.LogError("LetterPanel: LetterText was not found.");
+        }
+
+        if (wordsText == null)
+        {
+            Debug.LogError("LetterPanel: WordsText was not found.");
+        }
+    }
+
+    public void ShowLetter(string letter)
+    {
+        if (letterText == null || wordsText == null)
+        {
+            Debug.LogError("LetterPanel: Text fields are missing.");
+            return;
+        }
+
+        letter = letter.ToUpper();
+
+        letterText.text = letter;
+        wordsText.text = GetWords(letter);
+    }
+
+    private string GetWords(string letter)
+    {
+        switch (letter)
+        {
+            case "A":
+                return "Apple\nAnt\nAirplane\nAlligator";
+
+            case "B":
+                return "Ball\nBanana\nBird\nBook";
+
+            case "C":
+                return "Cat\nCar\nCake\nCow";
+
+            case "D":
+                return "Dog\nDuck\nDrum\nDolphin";
+
+            case "E":
+                return "Elephant\nEagle\nEgg\nEarth";
+
+            case "F":
+                return "Fish\nFrog\nFlower\nFox";
+
+            case "G":
+                return "Goat\nGrapes\nGuitar\nGorilla";
+
+            case "H":
+                return "Hat\nHorse\nHouse\nHoney";
+
+            case "I":
+                return "Ice Cream\nIgloo\nInsect\nIsland";
+
+            case "J":
+                return "Jam\nJacket\nJuice\nJellyfish";
+
+            case "K":
+                return "Kite\nKangaroo\nKey\nKing";
+
+            case "L":
+                return "Lion\nLemon\nLamp\nLeaf";
+
+            case "M":
+                return "Monkey\nMoon\nMango\nMilk";
+
+            case "N":
+                return "Nest\nNose\nNurse\nNut";
+
+            case "O":
+                return "Orange\nOwl\nOctopus\nOcean";
+
+            case "P":
+                return "Pencil\nPizza\nPenguin\nPanda";
+
+            case "Q":
+                return "Queen\nQuilt\nQuail\nQuestion";
+
+            case "R":
+                return "Rabbit\nRainbow\nRobot\nRose";
+
+            case "S":
+                return "Sun\nSnake\nStar\nSchool";
+
+            case "T":
+                return "Tiger\nTree\nTrain\nTable";
+
+            case "U":
+                return "Umbrella\nUniform\nUnicorn\nUncle";
+
+            case "V":
+                return "Van\nViolin\nVolcano\nVegetable";
+
+            case "W":
+                return "Whale\nWatch\nWatermelon\nWindow";
+
+            case "X":
+                return "Xylophone\nX-ray\nXerus\nXenon";
+
+            case "Y":
+                return "Yacht\nYo-yo\nYak\nYarn";
+
+            case "Z":
+                return "Zebra\nZoo\nZipper\nZero";
+
+            default:
+                return "No words found.";
+        }
+    }
+}
